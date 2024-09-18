fa215bee80333889f14902389b3772fafe1d3d7d
massiveattaque <kzramos@icloud.com>
2024-09-18 00:22:29 -0300

    cor ajustada

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function light() {
     document.documentElement.setAttribute('data-theme', 'pure');
     document.getElementById('weather').setAttribute('data-theme', 'pure');
     document.getElementById('weather').removeAttribute('data-basecolor');
-    document.getElementById('weather').setAttribute('data-textcolor', '#ff0202');
+    document.getElementById('weather').setAttribute('data-textcolor', '#000000');
     document.getElementById('weather').removeAttribute('data-cloudfill');
     document.getElementById('weather').setAttribute('data-suncolor', '#ff0202');
     script(document, 'script', 'weatherwidget-io-js');
